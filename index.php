e343d0c21917e5a9b42ecb275811e86409e3e76a
Jorge <jorgefreire2403@gmail.com>
2024-09-05 23:03:38 -0400

    Cambio 05/09

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include("template/cabecera.php");?>
+
+            <div class="jumbotron">
+                <h1 class="display-3">Bienvenidos</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+        
+<?php include("template/pie.php");?>
